307e39ca01fd3e7c5812234a52c4f245f0a46ea5
Unix <mb_wali@hotmail.com>
2021-03-03 16:27:03 +0100

    doi: wip minting new doi

---
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/DoiMint.js
@@ -0,0 +1,189 @@
+import React, { Component } from "react";
+import { Button, Card, Icon, Label, Loader, Dimmer, Message } from "semantic-ui-react";
+import { MapDatacite } from "./maptodatacite";
+import { DoiRest } from "./DoiRest";
+import { FieldArray } from "formik";
+
+
+export class DoiMint extends Component {
+  constructor(props) {
+    super(props);
+    this.record = props.record || {};
+    this.metadata = this.record.metadata;
+    this.is_doi = false;
+    this.id_doi = "";
+
+    // check for existing identifiers
+    if (
+      typeof this.metadata.identifiers != "undefined" &&
+      this.metadata.identifiers != null &&
+      this.metadata.identifiers.length != null &&
+      this.metadata.identifiers.length > 0 &&
+      this.metadata.identifiers[0] != null
+    ) {
+      this.is_doi = true;
+      this.id_doi = this.metadata.identifiers[0].identifier;
+    }
+
+    // add metadata to the state
+    this.state = {
+      identifiers: [],
+      showLoader: false,
+      doi_id: "",
+      errorMsg: "",
+      isError: false
+    };
+
+    // console.log(this.metadata);
+    // console.log("is publish?", this.record.is_published);
+    // console.log("is DOI?", this.is_doi);
+    // console.log("record id", this.record.id);
+    // console.log("state of metadata", this.state.metadata);
+  }
+
+  render() {
+    // this should fetch a new doi
+    var pushDoi = (form) => {
+      // activate the loader
+      this.setState({
+        showLoader: true,
+      });
+
+      // TODO: Get values from backend
+      // make request for a new DOI
+      const url = "https://api.test.datacite.org/dois"; // https://api.datacite.org/'
+      const auth = { username: "TUG.OPENLIB", password: "aaeidkeseldie783[]" };
+      const prefix = "10.0356"
+
+      // get mapped DOI
+      const mapped = MapDatacite(this.metadata, this.record.id, prefix);
+
+      const _doirest = new DoiRest(url);
+
+      // Create a new DOI
+      _doirest
+        .create(mapped, auth)
+        .then((data) => {
+          // if there is an error
+          if (data.data.errors) {
+            console.log(data.data.errors[0].title);
+            this.setState({
+              showLoader: false,
+              isError: true,
+              errorMsg: data.data.errors[0].title,
+            });
+            // new doi is fetched
+          } else {
+            // add new identifier
+            const _identifiers = [
+              {
+                identifier: data.data.data.id,
+                scheme: "doi",
+              },
+            ];
+
+            // submit the value to the form
+            this.setState({ identifiers: _identifiers });
+            form.setFieldValue("metadata.identifiers", this.state.identifiers);
+
+            this.is_doi = true;
+            // deactivate the loader
+            this.setState({
+              showLoader: false,
+              doi_id: this.state.identifiers[0].identifier,
+            });
+          }
+        })
+        .catch((error) => {
+          console.log("error", error);
+        });
+    };
+
+    // get a link of dio
+    var doiLink = (doiId) => {
+      window.open(`https://doi.org/${doiId}`, "_blank");
+    };
+
+    return (
+      <Card className="actions">
+        <Card.Content>
+          <Card.Header>
+            <Icon name="certificate" />
+            Datacite DOI
+          </Card.Header>
+          <Card.Description>
+            <span style={{ color: "#B6B6B6" }}>
+              Record must be a <b>published</b> record to mint a DOI.
+            </span>
+          </Card.Description>
+
+          {this.state.isError &&(
+          <Message negative>
+          <Message.Header>
+          {this.state.errorMsg}
+          </Message.Header>
+          <p>Please contact Repository supporters!</p>
+        </Message>
+          )}
+
+          {/* when the Component is rendered */}
+          {!this.is_doi && (
+            <FieldArray name="metadata.identifiers">
+              {(fieldArrayProps) => {
+                const { form } = fieldArrayProps;
+                return (
+                  <div
+                    style={{
+                      marginTop: "10px",
+                      textAlign: "center",
+                    }}
+                  >
+                    <Button
+                      compact
+                      className="save-button"
+                      disabled={!this.record.is_published}
+                      as={"label"}
+                      color="green"
+                      size="large"
+                      onClick={() => pushDoi(form)}
+                    >
+                      <Icon name="certificate" />
+                      Get DOI Now!
+                    </Button>
+                    {this.state.showLoader && (
+                      <Dimmer active inverted>
+                        <Loader inverted>Loading...</Loader>
+                      </Dimmer>
+                    )}
+                  </div>
+                );
+              }}
+            </FieldArray>
+          )}
+
+          {this.is_doi && (
+            <div
+              style={{
+                textAlign: "center",
+                marginTop: "10px",
+              }}
+            >
+              <Label
+                style={{
+                  cursor: "pointer",
+                }}
+                size="large"
+                as="a"
+                color="blue"
+                onClick={() => doiLink(this.state.doi_id || this.id_doi)}
+              >
+                <strong>DOI: </strong>
+                <Label.Detail>{this.state.doi_id || this.id_doi}</Label.Detail>
+              </Label>
+            </div>
+          )}
+        </Card.Content>
+      </Card>
+    );
+  }
+}
---
+++ invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/deposit/maptodatacite.js
@@ -0,0 +1,54 @@
+// WIP
+export function MapDatacite(metadata, recordid, prefix) {
+  // console.log('record id', recordid)
+  // console.log('retrieved', metadata)
+  // console.log('creators []', metadata.creators)
+  // console.log('publication_date []', metadata.publication_date)
+
+  // use this to generate random string
+  let r = Math.random().toString(36).substring(7);
+  const creators = []
+
+  // // func return first letter capit -
+  // const capitalize = (s) => {
+  //   if (typeof s !== 'string') return ''
+  //   return s.charAt(0).toUpperCase() + s.slice(1)
+  // }
+
+  // loop throgh and add the creators
+  if (metadata.creators){
+    for (const creator of metadata.creators){
+      creators.push({"name": creator.person_or_org.name})
+    }
+  }
+
+    const dataciterecord = {
+        data : {
+          id: prefix+"/datacite-"+recordid,
+          type: "dois",
+          attributes: {
+            event: "publish",
+            doi: prefix+"/datacite-"+recordid,
+            creators,
+            titles: [{
+              title: metadata.title
+            }],
+            publisher: metadata.publisher,
+            publicationYear: metadata.publication_date,
+            types: {
+              resourceTypeGeneral: "Text"
+            },
+            url: "https://repository.tugraz.at",
+            schemaVersion: "http://datacite-rest.org/schema/kernel-4"
+          }
+        }
+      };
+      return dataciterecord
+    }
+
+// docs: https://support.datacite.org/docs/api-create-dois
+// test created: https://api.test.datacite.org/dois/10.0356/datacite-8baxw
+
+// publish - Triggers a state move from draft or registered to findable
+// register - Triggers a state move from draft to registered
+// hide - Triggers a state move from findable to registered
